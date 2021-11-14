67bcc364022f5796d56482a35d8c84089b1b1e1e
smax253 <smax253@gmail.com>
2021-11-14 07:07:21 -0500

    got good icons

--- frontend/src/img/imageLogos.js
+++ frontend/src/img/imageLogos.js
@@ -3,7 +3,27 @@ import amazonLogo from "./amazon.png";
 import googleLogo from "./google.png";
 import netflixLogo from "./netflix.png";
 import salesforceLogo from "./salesforce.png";
+import airbnbLogo from "./airbnb.png";
+import amdLogo from "./amd.png";
+import appleLogo from "./apple.png";
+import ibmLogo from "./ibm.png";
+import instagramLogo from "./instagram.png";
+import intelLogo from "./intel.png";
+import microsoftLogo from "./microsoft.png";
+import paypalLogo from "./paypal.png";
+import squareLogo from "./square.png";
+import zoomLogo from "./zoom.png";
 const companyLogos = {
+  airbnb: airbnbLogo,
+  amd: amdLogo,
+  apple: appleLogo,
+  ibm: ibmLogo,
+  instagram: instagramLogo,
+  intel: intelLogo,
+  microsoft: microsoftLogo,
+  paypal: paypalLogo,
+  square: squareLogo,
+  zoom: zoomLogo,
   facebook: facebookLogo,
   amazon: amazonLogo,
   google: googleLogo,
